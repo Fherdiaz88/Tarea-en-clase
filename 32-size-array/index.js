052e69e89b1da0fd47278d8e1f3f4f634b24951d
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:56:57 -0500

    Add files via upload

---
+++ 32-size-array/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//crear una funcion que reciba un array y devuelva el numero de elementos
+
+function cont(a,b,c,d){
+    let sutm=0;
+    let fruits=["apple","orange","peach","banana"];
+    for (let sum = 0; sum < 4; sum++) {
+        constElement = fruits[sum];
+        
+    }
+
+};
+
+
+
+
+
+
