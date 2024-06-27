5dcb9228ad47775f0e705905bc46029975b865fa
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:39:09 -0500

    Add files via upload

---
+++ 06-Operadores/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let a = Number (prompt ("ingresar valor uno"));
+let b = Number (prompt ("ingresar valor dos"));
+
+//convertir el valor de 'a' a negativo
+
+let a2 = (-a);
+//alert (a2);
+
+a++;
+alert (a);
+
+b--;
+alert (b);
+
+++a;
+//alert (a);
+
+--b;
+//alert (b);
+
+a = ++a;
+//alert (a);
+
+b = --b;
+//alert (b);
+
+
+let result 
+
+result = (a+b);
+//alert (result);
+
+result = (a-b);
+//alert (result);
+
+result = (a*b);
+//alert (result);
+
+result = (a/b);
+//alert (result);
+
+result = (a%b);
+//alert (result);
