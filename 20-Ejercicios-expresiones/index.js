6f501dce1000ff869ed337177cdf7a71886d2c6a
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:50:07 -0500

    Add files via upload

---
+++ 20-Ejercicios-expresiones/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let yearOfBirth = prompt("Ingrese su año de nacimiento");
+function age(currentYear,yearOfBirth) {
+    
+    return result;
+}
+let result = (2024-yearOfBirth);
+alert(result);
