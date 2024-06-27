c70714e812e91b546647734dfd1539dea9e4aea7
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:56:06 -0500

    Add files via upload

---
+++ 25-Arrays/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let cars = ["Toyota","Mitsubishi","Subaru","Honda","Isuzu","Nissan","Mg","jac","jeep","land rover"];//subaru elemento 3, pero indice 2
+//Tamaño de array:9
+//¿Cual es el ultimo indice?:9
+//¿Cual es el primer indice?:0
+
+//inicializacion empieza en 0, condicion: tamaño del array,
+for(let i=0; i<10;i++){
+    if (i<7) {
+        alert("Toyota,Mitsubishi,Subaru,Honda,Isuzu y Nissan son marcas de carros confiables");
+    } 
+     else (i=>7); {
+        alert("Mg,jac,jeep y land rover son marcas de carros no confiables")
+}
+}
