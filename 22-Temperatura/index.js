5f5954bcd7d6d7460af7bef3b6b526d61232108c
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:52:22 -0500

    Add files via upload

---
+++ 22-Temperatura/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+let a= prompt("Ingrese la temperatura en grados celcius que desea convertir");
+let temperature =(a,b) => a*b;
+let result = temperature (a,33.8)
+alert(result + "grados Fahrenheit");
