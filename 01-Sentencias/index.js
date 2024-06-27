113542a0035d6a147abe007355c29e2971680f63
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:34:03 -0500

    Add files via upload

---
+++ 01-Sentencias/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//declaracion 
+let nickname = "fediaz";
+//asignacion
+let age;
+//asignacion
+age = "20";
+
+let height = "1860 mm";
+const birthdate = 31/10/2003;
+
+let isMarried = false;
+let children = null;
+let deathDate = undefined;
+let ceroSobreCero = undefined;
+let text = "bryan is a oldman";
+
+alert ('El nombre es: '+nickname); //El nombre es: fediaz
+
+alert ('Mi edad es: '+age + ', Mi estatura es: '+ height);
+
+alert (text)
