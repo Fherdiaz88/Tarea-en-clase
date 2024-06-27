0ce9fec5667c7ae7ae77179408850031eabc3f03
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:46:40 -0500

    Add files via upload

---
+++ 13-Multiplicacion/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function CalcMult(a,b) {
+    let result = 0;
+    for (let i =0; i < a; i++) {
+        result = result +b;
+    }
+    return result;
+}
+CalcMult(3,2);
+alert (result);
