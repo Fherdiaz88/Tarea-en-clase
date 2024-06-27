36fce8efd6556df60fc13c338cc82c3b133e41c8
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:56:43 -0500

    Add files via upload

---
+++ 31-array-string/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//let nums=[1,2,,3,4,5,6,7,8,9,0,12,24];
+let fruits=["apple","orange","peach","banana"];
+
+fruits.length;
+
+let institute = "Sudamericano";
+
+alert(institute[2])
+
+
+
+
+
+
+
+
+
