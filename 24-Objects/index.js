a6eb5c5c08d8422475aaa61a78c65268d52ae748
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:54:46 -0500

    Add files via upload

---
+++ 24-Objects/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let n= prompt ("Ingrese el nombre");
+let species= prompt ("Ingrese la especie");
+
+let level= Number (prompt ("Ingrese el nivel"));
+
+let pokemon = {
+    n,
+    species: [
+        articuno = {
+            tipo1: "Hielo",
+            tipo2: "Volador",
+        }, 
+        zapdos = {
+            tipo1: "Electrico",
+            tipo2: "Volador",
+        }, 
+        moltres = {
+            tipo1: "Fuego",
+            tipo2: "Volador",
+        }],
+    level,
+};
+
+alert (n, species, tipo1, tipo2, level)
