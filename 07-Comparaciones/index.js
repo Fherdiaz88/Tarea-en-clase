fc7470ef3c5e71ff7a6f9c8511509609d404cf7a
Fherdiaz88 <165815623+Fherdiaz88@users.noreply.github.com>
2024-06-27 10:41:26 -0500

    Add files via upload

---
+++ 07-Comparaciones/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let a = Number (prompt ("ingresar valor a"));
+let b = Number (prompt ("ingresar valor b"));
+
+//no muestra lo que esta en ""
+alert ("a>b " + a>b); 
+
+alert ("a<b " + a<b); 
+
+alert ("a>=b " + a>=b); 
+
+alert ("a<=b " + a<=b); 
+
+alert ("a==b " + a==b); 
+
+alert ("a===b " + a===b); 
+
+alert ("a!=b " + a!=b); 
+
+alert ("a!==b " + a!==b); 
